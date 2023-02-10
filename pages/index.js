fdf1147fd1e2b8164140e751bc7e8ff259346601
Eduardo Lopez <ed98lopez@gmail.com>
2023-02-09 21:28:40 -0800

    added keywords, and description meta tags

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,8 @@ export default function Home(props) {
   return (<>
     <Head>
     <title>Graphic Designer | Illustrator | Eddie Lopez | Fresno CA</title>
+    <meta name="keywords" content="graphic design, design, logo design, branding, central California, Fresno, portfolio, print"></meta>
+    <meta name='description' content="Portfolio of Central California based freelance graphic designer specializing in all areas of logo design, web design, print graphics. "/>
     </Head>
     <div>
       <GalleryList array={props.projects}/>
