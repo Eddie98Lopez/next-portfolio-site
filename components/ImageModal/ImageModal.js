1198dfc6cde983a1bb2e7ed23ddae3edf9e63f9f
Eduardo Lopez <ed98lopez@gmail.com>
2023-02-10 14:05:27 -0800

    initial commit and brainstormed comments in Image Modal component folder

---
+++ components/ImageModal/ImageModal.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+/*
+
+This component will be visible when the user clicks on an image. 
+
+Features: 
+1. Close Button
+2. Wrapper with darkened and blurred background 
+3. Buttons that scroll to next image in project 
+
+*/
+
+const ImageModal = () => {
+  return (
+    <div>ImageModal</div>
+  )
+}
+
+export default ImageModal
