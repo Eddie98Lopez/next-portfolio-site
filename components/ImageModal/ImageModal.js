ae10f9a5f18e680e9942e634efd5c9f348dc35e5
Eduardo Lopez <ed98lopez@gmail.com>
2023-02-11 14:46:34 -0800

    finished styling and functionality of the image modal

--- components/ImageModal/ImageModal.js
+++ components/ImageModal/ImageModal.js
@@ -1,8 +1,18 @@
-import Image from 'next/image'
-import React , {useState} from 'react'
-import { ModalContent, ModalWrapper } from './ImageModal.styles'
-import { useImgIndex, useDispatch, useModal } from '../ProjectDetails/ProjectProvider'
-
+import Image from "next/image";
+import React, { useState } from "react";
+import {
+  ModalContent,
+  ModalWrapper,
+  TraverseButton,
+  CloseButton,
+  Buttons,
+} from "./ImageModal.styles";
+import {
+  useImgIndex,
+  useDispatch,
+  useModal,
+} from "../ProjectDetails/ProjectProvider";
+import ToTopIcon from "../ScrollToTop/backToTop";
 
 /*
 
@@ -21,33 +31,47 @@ Features:
 */
 
 const ImageModal = (props) => {
-    const {array,display} = props
-    const active = useImgIndex()
-    const dispatch = useDispatch()
-    const modal = useModal()
-    const close = () => dispatch({type:"TOGGLE_MODAL"})
-    const updateImgIndex = (payload) => dispatch({type:'UPDATE_IMG_INDEX', payload:payload })
+  const { array, display } = props;
+  const active = useImgIndex();
+  const dispatch = useDispatch();
+  const modal = useModal();
+  const close = () => dispatch({ type: "TOGGLE_MODAL" });
+  const updateImgIndex = (payload) =>
+    dispatch({ type: "UPDATE_IMG_INDEX", payload: payload });
 
+  const goPrev = () => {
+    active === 0
+      ? updateImgIndex(array.length - 1)
+      : updateImgIndex(active - 1);
+  };
+  const goNext = () => {
+    active + 1 > array.length - 1
+      ? updateImgIndex(0)
+      : updateImgIndex(active + 1);
+  };
 
-    const goPrev = () => {
-        active === 0 ?updateImgIndex(array.length - 1) :updateImgIndex(active - 1) 
-    }
-    const goNext = () => {
-        active + 1 > array.length - 1 ? updateImgIndex(0) : updateImgIndex(active+1)  
-    }
-
-
-    console.log(props.array[active])
+  console.log(props.array[active]);
   return (
     <ModalWrapper display={modal.toString()}>
-        <ModalContent>
-            <button onClick={close}>close</button>
-            <div><img src={array[active].href} alt='projectimage'/></div>
-            <button onClick={goPrev}>prev</button>
-            <button onClick={goNext}>next</button>
-        </ModalContent>
+      <ModalContent>
+        <CloseButton onClick={close}>X</CloseButton>
+        <div>
+          <img src={array[active].href} alt="projectimage" />
+        </div>
+        <Buttons>
+          <TraverseButton rotate="-90deg" onClick={goPrev}>
+            <ToTopIcon />
+          </TraverseButton>
+          <div>
+            {active + 1}/{array.length}
+          </div>
+          <TraverseButton rotate="90deg" onClick={goNext}>
+            <ToTopIcon />
+          </TraverseButton>
+        </Buttons>
+      </ModalContent>
     </ModalWrapper>
-  )
-}
+  );
+};
 
-export default ImageModal
+export default ImageModal;
--- components/ImageModal/ImageModal.styles.js
+++ components/ImageModal/ImageModal.styles.js
@@ -1,29 +1,37 @@
 import styled from 'styled-components'
+import ScrollButton from '../ScrollToTop/ScrollToTop.styles'
 
 export const ModalWrapper = styled.div`
 height: 100vh;
 width: 100%;
 position:fixed;
+box-sizing:border-box;
 top:0;
 left:0;
+margin:auto;
+text-align:center;
 z-index: 600000000000000000000000;
-background: rgba(0,0,0,.5);
-backdrop-filter:blur(3px);
+background: rgba(0,0,0,.65);
+backdrop-filter:blur(7px);
 display:${props => props.display === 'true' ? 'grid' : 'none'};
-place-content: center center;
+
 `
 
 export const ModalContent = styled.div`
-width: 90%;
-height: 90vh;
-background: black;
+width: 100%;
+height: 85vh;
+margin:auto;
+
 box-sizing: border-box;
+display:grid;
+gap:1rem;
 
 
-& div {
+& div:nth-child(2) {
     object-fit:contain;
-    
+    box-sizing:border-box;
     height:inherit;
+    
 & img{
     height:100%;
     width:auto;
@@ -31,5 +39,47 @@ box-sizing: border-box;
 }
 }
 
+`
+
+export const CloseButton = styled.button`
+position:absolute;
+top:1.5rem;
+right: 1.5rem;
+color: white;
+width: 1.5rem;
+height: 1.5rem;
+text-align:center;
+border: white solid 2px;
+background: none;
+border-radius:300px;
+`
+
+export const TraverseButton = styled.button`
+height:1.5rem;
+width:1.5rem;
+box-sizing:border-box;
+padding:0;
+margin:0;
+border:none;
+border-radius:200px;
+color:white;
+outline:none;
+background:none;
+
+& svg{
+    fill:white;
+    transform:rotate(${props=>props.rotate});
+    height:inherit;
+    width:auto;}`
+
+
+export const Buttons = styled.div`
+box-sizing: border-box;
+width:100%;
+display:flex;
+gap: 2rem;
+place-content:center center;
+color:white;
+font-family:sans-serif;
 `
 
