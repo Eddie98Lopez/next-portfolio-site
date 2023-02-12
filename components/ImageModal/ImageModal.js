17fc55dc696946be96b9fd83b100752f257ae55c
Eduardo Lopez <ed98lopez@gmail.com>
2023-02-12 09:49:10 -0800

    added a close button svg icon. Made the travers buttons more mobile friendly. They enlarge at smaller media widths.

--- components/ImageModal/ImageModal.js
+++ components/ImageModal/ImageModal.js
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import React, { useState } from "react";
+import CloseIcon from "./closeIcon";
 import {
   ModalContent,
   ModalWrapper,
@@ -13,22 +13,6 @@ import {
   useModal,
 } from "../ProjectDetails/ProjectProvider";
 import ToTopIcon from "../ScrollToTop/backToTop";
-
-/*
-
-This component will be visible when the user clicks on an image. 
-
--Will need a list of images from props
--will need an "active image index" slice of state
--will need "next" and "prev" helper functions that update the active image index
-    -- !array[i+1] && array[0] 
-
-Features: 
-1. Close Button
-2. Wrapper with darkened and blurred background 
-3. Buttons that scroll to next image in project 
-
-*/
 
 const ImageModal = (props) => {
   const { array, display } = props;
@@ -54,7 +38,11 @@ const ImageModal = (props) => {
   return (
     <ModalWrapper display={modal.toString()}>
       <ModalContent>
-        <CloseButton onClick={close}>X</CloseButton>
+        <CloseButton onClick={close}>
+        
+                <CloseIcon/>
+            
+        </CloseButton>
         <div>
           <img src={array[active].href} alt="projectimage" />
         </div>
