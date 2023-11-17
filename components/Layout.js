edf99d4c3095751ae43a1f6550b0d8ae4f12bf4a
Eduardo <ed98lopez@gmail.com>
2023-11-16 23:20:52 -0800

    font

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Navigation from './Navigation/Navigation'
 import Footer from './Footer'
 import Loading from './Loading'
-import 'animate.css'
+import '../styles/globals.css'
 
 
 const Layout = ({children, ...props}) => {
